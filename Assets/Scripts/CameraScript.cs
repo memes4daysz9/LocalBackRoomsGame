2115f1ae205a0c1baaaed24b78ba60519eabfb6d
memes4daysz9 <benj08082008@icloud.com>
2023-03-09 17:01:17 -0800

    First Commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public Vector2 Mouseturn;
+    public float  sesitivity = 20.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mouseturn.x += Input.GetAxis("Mouse X") * sesitivity;
+        Mouseturn.y += Input.GetAxis("Mouse Y") * sesitivity;
+        transform.localRotation = Quaternion.Euler(-Mouseturn.y, Mouseturn.x,0);
+    }
+}
