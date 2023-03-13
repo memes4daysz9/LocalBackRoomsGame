f1fe6c2ad01bdf75a1d405962004ec3683a90d3b
muffin039483 <vwmetz08@gmail.com>
2023-03-13 13:36:51 -0700

    E

--- Assets/Scripts/CanvasScript.cs
+++ Assets/Scripts/CanvasScript.cs
@@ -16,16 +16,16 @@ using UnityEngine;
              if(Paused == true){
                  Time.timeScale = 1.0f;
                  Canvas.gameObject.SetActive (false);
-                 Screen.showCursor = false;
+                 Cursor.visible = false;
                  Screen.lockCursor = true;
-                 Camera.audio.Play ();
+                 Camera.GetComponent<AudioSource>().Play ();
                  Paused = false;
              } else {
                  Time.timeScale = 0.0f;
                  Canvas.gameObject.SetActive (true);
-                 Screen.showCursor = true;
+                 Cursor.visible = true;
                  Screen.lockCursor = false;
-                 Camera.audio.Pause ();
+                 Camera.GetComponent<AudioSource>().Pause ();
                  Paused = true;
              }
          }
@@ -33,8 +33,8 @@ using UnityEngine;
      public void Resume(){
          Time.timeScale = 1.0f;
          Canvas.gameObject.SetActive (false);
-         Screen.showCursor = false;
+         Cursor.visible = false;
          Screen.lockCursor = true;
-         Camera.audio.Play ();
+         Camera.GetComponent<AudioSource>().Play ();
      }
  }   
